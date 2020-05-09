68d896c4194c61d4cc34faefc683aecc89d2efd2
Augustine Okore <aookore@unionbankng.com>
2020-05-09 12:00:17 +0100

    commit django starter

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from pages import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+     path('about', views.about, name='about'),
+
+]
--- stcharlesproject/wsgi.py
+++ stcharlesproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for _djproject project.
+WSGI config for stcharlesproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_djproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stcharlesproject.settings')
 
 application = get_wsgi_application()
